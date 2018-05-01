0aa5b90ee0b5352f077be5bed29f39e1c39ab88b
Steven Chen <ysteven.chen2@gmail.com>
2018-05-01 05:34:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ if string=="I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   "I LOVE YOU TOO PUMPKIN!"
 elsif string== string.upcase
-    puts "NO, NOT SINCE 1983!"
-    "NO, NOT SINCE 1983!"
+    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
     "HUH?! SPEAK UP, SONNY!"
